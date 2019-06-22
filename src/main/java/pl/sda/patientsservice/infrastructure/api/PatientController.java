b98c876e638767310ac195fe1525b32959f2cd13
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2019-06-22 13:07:46 +0200

    fix

--- src/main/java/pl/sda/patientsservice/infrastructure/api/PatientController.java
+++ src/main/java/pl/sda/patientsservice/infrastructure/api/PatientController.java
@@ -9,42 +9,42 @@ import pl.sda.patientsservice.infrastructure.dto.PatientDto;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/patient")
-@RequiredArgsConstructor
-public class PatientController {
+    @RestController
+    @RequestMapping("/patient")
+    @RequiredArgsConstructor
+    public class PatientController {
 
-    private final PatientFinder patientFinder;
-    private final PatientService patientService;
+        private final PatientFinder patientFinder;
+        private final PatientService patientService;
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PatientDto> getOne(@PathVariable Long id) {
-        return patientFinder.findOne(id).map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
+        @GetMapping("/{id}")
+        public ResponseEntity<PatientDto> getOne(@PathVariable Long id) {
+            return patientFinder.findOne(id).map(ResponseEntity::ok)
+                    .orElseGet(() -> ResponseEntity.notFound().build());
+        }
 
-    @GetMapping
-    public ResponseEntity<List<PatientDto>> getAll() {
-        return ResponseEntity.ok(patientFinder.findAll());
-    }
+        @GetMapping
+        public ResponseEntity<List<PatientDto>> getAll() {
+            return ResponseEntity.ok(patientFinder.findAll());
+        }
 
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody PatientDto patientDto) {
-        patientService.create(patientDto);
-        return ResponseEntity.status(201).build();
-    }
+        @PostMapping
+        public ResponseEntity<Void> create(@RequestBody PatientDto patientDto) {
+            patientService.create(patientDto);
+            return ResponseEntity.status(201).build();
+        }
 
-    @PutMapping
-    public ResponseEntity<Void> update(@RequestBody PatientDto patientDto) {
-        patientService.edit(patientDto);
-        return ResponseEntity.status(200).build();
-    }
+        @PutMapping
+        public ResponseEntity<Void> update(@RequestBody PatientDto patientDto) {
+            patientService.edit(patientDto);
+            return ResponseEntity.status(200).build();
+        }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        patientService.delete(id);
-        return ResponseEntity.status(204).build();
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> delete(@PathVariable Long id) {
+            patientService.delete(id);
+            return ResponseEntity.status(204).build();
 
-    }
+        }
 }
